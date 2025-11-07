32ada3b892bceb3da8c04eda7f8f0eaf998b28bc
IEVN1002-22001432 <80981@alumnos.utleon.edu.mx>
2025-11-07 14:10:40 -0600

    Update main.py

--- main.py
+++ main.py
@@ -20,6 +20,8 @@ USER_MAP = {
     "79028@alumnos.utleon.edu.mx": "usuarioGitHub",
 }
 
+print(f"https://api.github.com/repos/{GITHUB_OWNER}/{GITHUB_REPO}/issues");
+
 # ---------------- Azure Boards ---------------- #
 
 @app.post("/update")
